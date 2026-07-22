1a00e1e1ea5bc8453788c9be134a9a3c666d3b1c
Abel <abeladigun11@gmail.com>
2026-07-22 03:59:35 +0100

    fix: inject nextauth environment variables into next config to resolve vercel server error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "akpaka-super-secret-development-key-2026",
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000"),
+  }
 };
 
 export default nextConfig;
